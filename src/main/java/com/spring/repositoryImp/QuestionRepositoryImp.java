81f96a2c9873c0f9f9ab9e4ee6db0d284f4665a2
vanthang1996 <vanthang20437@gmail.com>
2018-05-28 14:26:42 +0700

    Thang123

--- src/main/java/com/spring/repositoryImp/QuestionRepositoryImp.java
+++ src/main/java/com/spring/repositoryImp/QuestionRepositoryImp.java
@@ -1,7 +1,10 @@
 package com.spring.repositoryImp;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -10,6 +13,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.spring.mapper.entities.Subject;
 import com.spring.repository.QuestionRepository;
 
 @Repository
@@ -31,5 +35,29 @@ public class QuestionRepositoryImp implements QuestionRepository {
 
 		}
 		return list;
+	}
+
+	@Override
+	public Optional<?> getListQuestionByChapterIdPaging(long chapterId, int page, int size) {
+		SqlSession session = sessionFactory.openSession();
+		List<Subject> list = null;
+		Map<String, Object> param = new HashMap<>();
+		Map<String, Object> result = new HashMap<>();
+		param.put("page", page);
+		param.put("size", size);
+		param.put("chapterId", chapterId);
+		try {
+			list = session.selectList("com.spring.mapper.QuestionMapper.getListQuestionByChapterIdPaging", param);
+			int numberOfPage = (int) param.get("sumPage");
+			int numberOfRecord = (int) param.get("sumRecord");
+			result.put("listOfResult", list);
+			result.put("numberOfPage", numberOfPage);
+			result.put("numberOfRecord", numberOfRecord);
+		} catch (Exception e) {
+			logger.error("[getListQuestionByChapterIdPaging() is ERROR]" + e.getMessage());
+		} finally {
+			session.close();
+		}
+		return Optional.ofNullable(result);
 	}
 }
