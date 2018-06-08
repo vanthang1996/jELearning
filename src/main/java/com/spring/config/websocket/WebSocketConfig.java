524ad1d97ce9ca12c5c502c0f036702ac10d804d
vanthang1996 <vanthang20437@gmail.com>
2018-06-09 00:08:58 +0700

    Thang123

--- src/main/java/com/spring/config/websocket/WebSocketConfig.java
+++ src/main/java/com/spring/config/websocket/WebSocketConfig.java
@@ -16,7 +16,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
-		config.enableSimpleBroker("/queue", "/topic", "/listen-change-res");
+		config.enableSimpleBroker("/queue");
 		config.setApplicationDestinationPrefixes("/app");
 	}
 
